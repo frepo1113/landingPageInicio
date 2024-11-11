addc20daced0823822d24b61876dee0f93890f71
kellyflechas <kellyflechas1008@gmail.com>
2024-11-11 16:02:20 -0500

    inicio como mis cards

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css';
+import CardPaciente from './components/cards/cardPaciente'; 
+
+function App() {
+  
+  return (
+    
+      <div>
+        <CardPaciente/>
+        
+      </div>
+      
+  )
+}
+
+export default App
---
+++ src/components/cards/cardPaciente.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const cardPaciente = () => {
+  return (
+    <div>cardPaciente</div>
+  )
+}
+
+export default cardPaciente
